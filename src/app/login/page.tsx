db780c863da7f0b378053ba004bcd11c7c5f75fc
MockingJayTC <victoraremu123@gmail.com>
2023-08-05 02:24:01 +0000

    layout added

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1 className="text-3xl text-black">Login</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Signup</title>
+      </Head>
+      <h1>Signup</h1>
+    </div>
+  );
+};
+
+export default SignupPage;
